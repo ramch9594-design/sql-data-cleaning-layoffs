df471e835ee3b075f71e1e357d5a80fe4c397b27
Chaganti Ramanjaneyulu <ramch9594@gmail.com>
2026-03-25 19:53:16 +0530

    Create layoffsr table for raw data storage

---
+++ sql/01_create_table.sql
@@ -0,0 +1,13 @@
+-- Create base table for raw data
+
+CREATE TABLE layoffsr (
+    company TEXT,
+    location TEXT,
+    industry TEXT,
+    total_laid_off TEXT,
+    percentage_laid_off TEXT,
+    "date" TEXT,
+    stage TEXT,
+    country TEXT,
+    funds_raised_millions TEXT
+);
